87a8e8a6367d3245121bac8c4d26868d8e66fc5f
Aleksandr <titamoto@gmail.com>
2023-09-17 11:59:07 -0700

    passed test

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,19 +8,131 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        if 'username' not in json:
+            return make_response({}, 422)
+        user = User(
+            username=json['username'],
+            image_url=json['image_url'],
+            bio=json['bio']
+        )
+        user.password_hash = json['password']
+        db.session.add(user)
+        db.session.commit()
+        user_record = User.query.filter(User.username == user.username).first()
+        if not user_record:
+            return make_response({}, 422)
+        session['user_id'] = user_record.id
+        return make_response({
+            "id": user_record.id,
+            "username": user_record.username,
+            "image URL": user_record.image_url,
+            "bio": user_record.bio
+        }, 201)
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            user_record = User.query.filter(User.id == session['user_id']).first()
+            if not user_record:
+                return make_response({}, 422)
+            return make_response({
+            "id": user_record.id,
+            "username": user_record.username,
+            "image URL": user_record.image_url,
+            "bio": user_record.bio
+        }, 200)
+        return make_response({}, 401)
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        if 'username' not in json:
+            return make_response({}, 401)
+        username = json['username']
+        password = json['password']
+        user = User.query.filter(User.username == username).first()
+        if not user:
+            return make_response({}, 401)
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return make_response({
+            "id": user.id,
+            "username": user.username,
+            "image URL": user.image_url,
+            "bio": user.bio
+        }, 200)
+        return make_response({}, 401)
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return make_response({'message': '204: No Content'}, 204)
+        session['user_id'] = None
+        return make_response({}, 401)
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            recipe_list = []
+            for recipe in user.recipes:
+                recipe_dict = {
+                "title": recipe.title,
+                "instructions": recipe.instructions,
+                "minutes_to_complete": recipe.minutes_to_complete,
+                }
+                recipe_list.append(recipe_dict)
+            return make_response(
+                jsonify(recipe_list),
+                200
+            )
+
+            # recipes = [recipe.to_dict() for recipe in Recipe.query.filter(Recipe.user_id == user_id).all()]
+            # return make_response(jsonify(recipes), 200)
+        return make_response({}, 401)
+    
+    def post(self):
+        if not session.get('user_id'):
+            return make_response({}, 401)
+        recipe_form = request.get_json()
+        if 'title' not in recipe_form:
+            return make_response({}, 422)
+        if 'instructions' not in recipe_form:
+            return make_response({}, 422)
+        if len(recipe_form['instructions']) < 50:
+            return make_response({}, 422)            
+        recipe = Recipe(
+            title = recipe_form['title'],
+            instructions = recipe_form['instructions'],
+            minutes_to_complete = recipe_form['minutes_to_complete']
+        )
+        recipe.user_id = session['user_id']
+        db.session.add(recipe)
+        db.session.commit()
+
+        recipe_record = Recipe.query.filter(Recipe.user_id == session['user_id']).filter(Recipe.title == recipe.title).first()
+        if not recipe_record:
+            return make_response({}, 422)
+        user_record = User.query.filter(User.id == session['user_id']).first()
+        return make_response(
+            {
+                "title": recipe_record.title,
+                "instructions": recipe_record.instructions,
+                "minutes_to_complete": recipe_record.minutes_to_complete,
+                "user": {
+            "id": user_record.id,
+            "username": user_record.username,
+            "image URL": user_record.image_url,
+            "bio": user_record.bio
+                }
+            },
+            201
+        )
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -1,14 +1,67 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import CheckConstraint
+from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String(60))
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
-    pass
+    recipes = db.relationship('Recipe', backref='user')
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username: 
+            raise ValueError('Username is required')
+        return username
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}, image: {self.image_url}, bio: {self.bio}'
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if title == None:
+            raise ValueError('Title is required')
+        return title
+    
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if instructions == None:
+            raise ValueError('Instructions are required')
+        return instructions
+
+    def __repr__(self):
+        return f'Recipe {self.title}, ID: {self.id}, instructions: {self.instructions}, minutes to complete: {self.minutes_to_complete}'
